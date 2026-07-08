74b510a9aa92d3aeda311b8bc5c2f8b728f961b8
hoakiin <millychak89@gmail.com>
2026-07-08 14:34:08 +0300

    fix: configure base path for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router"
 import { store } from "./app/store"
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/todolist">
     <Provider store={store}>
       <App />
     </Provider>
